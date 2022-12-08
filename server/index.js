a48cdb295150c3226fb8b7c3b34502b8ba88edb3
Fawaz <fawaztotonchy2@gmail.com>
2022-12-08 18:05:29 +0800

    make it better

--- server/index.js
+++ server/index.js
@@ -84,6 +84,6 @@ app.put("/api/update/:id", (req, res) => {
 // });
 
 
-app.listen( 5000, () => {
+app.listen(process.env.PORT || 5000, () => {
     console.log("server is running on port 5000")
 })
